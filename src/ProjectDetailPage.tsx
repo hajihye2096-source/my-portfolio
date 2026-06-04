03ad2c72ecff466d8ced231817b96073000a17a6
hajihye <hajihye2096@gmail.com>
2026-06-04 12:19:18 +0900

    Center project one variant note text

--- src/ProjectDetailPage.tsx
+++ src/ProjectDetailPage.tsx
@@ -427,7 +427,7 @@ const VariantSection = () => (
         <SvgAsset src={assets.variantBTop} alt="Version B 목업" className="w-[375px]" />
       </FadeUp>
       <FadeUp delay={0.1} className="max-w-[1276px] mx-auto mt-12">
-        <Body>디자인팀은 배달현황 확인을 덜 방해하는 B안을 추천했지만, 초기 리더십의 방향성은 가시성이 가장 높은 C안이었습니다.</Body>
+        <Body className="text-center">디자인팀은 배달현황 확인을 덜 방해하는 B안을 추천했지만, 초기 리더십의 방향성은 가시성이 가장 높은 C안이었습니다.</Body>
       </FadeUp>
       <FadeUp delay={0.12} className="max-w-[1280px] mx-auto mt-14 grid xl:grid-cols-[790px_375px] gap-10 xl:gap-[114px] items-start justify-center">
         <SvgAsset src={assets.variantCompare} alt="Version B 비교 목업" className="w-[790px]" />
